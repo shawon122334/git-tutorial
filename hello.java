93195c6107f57b756816e48831c2bd012b1d23f1
Sabbir Ahmed Shawon <shawon.mu11@gmail.com>
2020-11-11 13:41:47 +0600

    added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,10 @@
 public class Hello {
     public static void main (string[] args){
-        system.out.print("this is a simple java code ")
+        system.out.println("this is a simple java code ");
+        display("hello man")
+    }
+    publinc static void display(string name){
+        system.out.println("this is display function" name);
     }
 
 }
